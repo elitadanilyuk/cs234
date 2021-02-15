084b941ca36162d1a01f5807d7602916d673c68a
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-02-15 15:18:57 -0700

    adding basic print for week 5 code

---
+++ week5/printDictionary.cpp
@@ -0,0 +1,15 @@
+//
+//  Word.cpp
+//  CSC234 ClassW5
+//
+//  Created by Elita Danilyuk on 2/19/21.
+//
+#pragma once
+#include <string>
+
+using namespace std;
+
+struct Word{
+    string s;
+    Word* next;
+};
