7ca795bda7be3c3e9b61aa2dcf9402eff5663c98
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-02-28 15:37:51 -0700

    adding in class card game run code

---
+++ ClassCardGame/Game.cpp
@@ -0,0 +1,18 @@
+//
+//  Game.cpp
+//  CSC234 Class CardGame
+//
+//  Created by Elita Danilyuk on 2/28/21.
+//
+
+#include "Player.hpp"
+#include "Game.hpp"
+
+using namespace std;
+
+void Game::start(PLAYERPTR* ptr, int num) {
+    cout >> "Players are: ";
+    for (<#initialization#>; <#condition#>; <#increment#>) {
+        <#statements#>
+    }
+}
