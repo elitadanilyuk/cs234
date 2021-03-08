d1c260606693569b146f5817e6a8a67c29968888
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-03-08 12:15:53 -0700

    adding hand class methods to ClassCardGame

---
+++ ClassCardGame/Hand.cpp
@@ -0,0 +1,20 @@
+//
+//  Hand.cpp
+//  CSC234 Class CardGame
+//
+//  Created by Elita Danilyuk on 2/28/21.
+//
+
+#include "Hand.hpp"
+
+void Hand::sort(){
+    
+}
+
+int Hand::discard(){
+    return 0;
+}
+
+int Hand::score(){
+    return 0;
+}
