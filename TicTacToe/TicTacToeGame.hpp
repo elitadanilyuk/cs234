ea807e9a520a168c675e46528d84921160c5873c
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-03-26 15:10:21 -0700

    creating and adding TicTacToe

---
+++ TicTacToe/TicTacToeGame.hpp
@@ -0,0 +1,28 @@
+////
+////  TicTacToeGame.hpp
+////  CSC234 TicTacToe
+////
+////  Created by Elita Danilyuk on 3/28/21.
+////
+//#include <string>
+//#pragma once
+//
+//using namespace std;
+//
+//class TicTacToeGame{
+//public:
+//    TicTacToeGame(); //constructor
+//    void playGame(); // plays the game
+//private:
+//    void clearBoard(); // clears the board
+//    void printBoard(); // print the board
+//    int getXCoord(); //gets input from user
+//    int getYCoord();
+//    bool placeMarker(int x, int y, char currentPlayer);
+//    bool checkForVictory(); //checks row, columns, and diagonals
+//    char board [n][m];// nxm array
+//};
+//
+//bool placeMarker(x, y, currentPlayer){
+//    
+//}
