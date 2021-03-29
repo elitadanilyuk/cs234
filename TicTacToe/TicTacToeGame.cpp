eb16efa4ce0475b39edbbcf32d201bbcff3a3bad
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-03-28 22:20:31 -0700

    adding tic tac toe game class methods

---
+++ TicTacToe/TicTacToeGame.cpp
@@ -0,0 +1,37 @@
+////
+////  TicTacToeGame.cpp
+////  CSC234 TicTacToe
+////
+////  Created by Elita Danilyuk on 3/28/21.
+////
+//#include "TicTacToeGame.hpp"
+//
+//void TicTacToeGame::playGame(){
+//    //precondtion: board has been cleared
+//    //postcondtion: game over; winner has been announced
+//    while(isDone == false){//game loop
+//        //play the game
+//        printBoard();
+//        x = getXCoord();
+//        y = getYCoord();
+//        placeMarker(x,y);
+//        checkForVictory();
+//        
+//    }
+//    
+//}
+//
+//void TicTacToeGame::clearBoard(){
+//    // precondition: board has been allocated
+//    //postcondition: board elements reset to <blank>
+//}
+//    
+//void TicTacToeGame::checkForVictory(){
+//    //precondition: board has been allocated
+//    //postcondition: board is unaltered
+//    
+//    //check the rows
+//    //check the columns
+//    //check right diagonal
+//    //check left diagonal
+//}
