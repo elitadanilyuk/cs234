7a3378164dc077c945578c0dbdc33fe84fb34e00
Elita Danilyuk <elitadanilyuk@gmail.com>
2021-04-23 15:16:22 -0700

    adding classLab7 hello world

---
+++ ClassLab7.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ClassLab7.0
+//
+//  Created by Elita Danilyuk on 4/23/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
